c7dfa672abd443f553061a5d7578e968574fbd06
Marestoni <46625695+Marestoni@users.noreply.github.com>
2020-03-14 09:54:18 -0300

    Update firebase.js
    Você pode achar acessando do console do firebase, indo em Configurações do projeto>Geral, basta descer até achar Firebase SDK snippet, lá tem a configuração de acesso que colocara no arquivo do diretorio src/config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD9cqQf5qLu4qdv2SR3mAFsk6K5qavUCNs",
-    authDomain: "acervo-eed8e.firebaseapp.com",
-    databaseURL: "https://acervo-eed8e.firebaseio.com",
-    projectId: "acervo-eed8e",
-    storageBucket: "acervo-eed8e.appspot.com",
-    messagingSenderId: "432819294013",
-    appId: "1:432819294013:web:7fc74b651797344b567f54"
+    //aqui coloque sua configuração de seu firebase
   };
 
   export default firebase.initializeApp(firebaseConfig);
